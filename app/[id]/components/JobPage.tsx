d741bdda5d6ca4a4ba66c1dc4a3ce7440c4c3313
Jared Brown <jaredbrown1@hotmail.com>
2024-01-10 20:50:49 -0800

    job page header moved to own component file

--- app/[id]/components/JobPage.tsx
+++ app/[id]/components/JobPage.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+import JobPageHeader from "./JobPageHeader";
 
 interface JobPageProps {
 	params: {
@@ -29,29 +28,7 @@ const JobPage = ({ params }: JobPageProps) => {
 
 	return (
 		<div className="flex flex-col justify-center items-center">
-			<div className="w-[730px] h-[140px] flex bg-white rounded-md">
-				<div className="w-[140px]">
-					<Image
-						src={params.logo}
-						alt="company logo"
-						width={140}
-						height={140}
-					/>
-				</div>
-				<div className="flex justify-between w-[590px] p-12">
-					<div>
-						<h1 className="text-2xl font-bold">{params.company}</h1>
-						<p>{params.company}.com</p>
-					</div>
-					<div>
-						<Link href={params.website}>
-							<button className="bg-[#5964E0] text-[#5964E0] bg-opacity-[0.2] w-[147px] h-[48px] rounded-md font-bold">
-								Company Site
-							</button>
-						</Link>
-					</div>
-				</div>
-			</div>
+			<JobPageHeader params={params} />
 			<div>
 				<div>
 					<div>
