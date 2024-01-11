9c3df4aee66e282d74bdb8db4115c1ee1748d159
Jared Brown <jaredbrown1@hotmail.com>
2024-01-10 20:45:37 -0800

    job page header for desktop completed

--- app/[id]/components/JobPage.tsx
+++ app/[id]/components/JobPage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface JobPageProps {
 	params: {
@@ -7,6 +8,8 @@ interface JobPageProps {
 		position: string;
 		location: string;
 		description: string;
+		company: string;
+		website: string;
 		logo: string;
 		requirements: {
 			content: string;
@@ -25,9 +28,9 @@ const JobPage = ({ params }: JobPageProps) => {
 	}
 
 	return (
-		<div>
-			<div className="w-[730px] h-[140px] ">
-				<div>
+		<div className="flex flex-col justify-center items-center">
+			<div className="w-[730px] h-[140px] flex bg-white rounded-md">
+				<div className="w-[140px]">
 					<Image
 						src={params.logo}
 						alt="company logo"
@@ -35,7 +38,19 @@ const JobPage = ({ params }: JobPageProps) => {
 						height={140}
 					/>
 				</div>
-				<div></div>
+				<div className="flex justify-between w-[590px] p-12">
+					<div>
+						<h1 className="text-2xl font-bold">{params.company}</h1>
+						<p>{params.company}.com</p>
+					</div>
+					<div>
+						<Link href={params.website}>
+							<button className="bg-[#5964E0] text-[#5964E0] bg-opacity-[0.2] w-[147px] h-[48px] rounded-md font-bold">
+								Company Site
+							</button>
+						</Link>
+					</div>
+				</div>
 			</div>
 			<div>
 				<div>
