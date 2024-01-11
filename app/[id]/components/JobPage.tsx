5024b2d2a60f22e4215c757fde736f2c17e801fb
Jared Brown <jaredbrown1@hotmail.com>
2024-01-10 22:34:10 -0800

    jobpage working just needs styling to be finished

--- app/[id]/components/JobPage.tsx
+++ app/[id]/components/JobPage.tsx
@@ -29,33 +29,49 @@ const JobPage = ({ params }: JobPageProps) => {
 	return (
 		<div className="flex flex-col justify-center items-center">
 			<JobPageHeader params={params} />
-			<div className="w-[730px] h-[1257px] flex bg-white rounded-md flex-col mt-10">
-				<div>
+			<div className="w-[730px] h-[1257px] flex bg-white rounded-md flex-col mt-10 p-[48px]">
+				<div className="flex justify-between">
 					<div>
-						<p>{params.postedAt}</p>
-						<p>{params.contract}</p>
+						{" "}
+						<div className="flex">
+							<p>{params.postedAt}</p>
+							<p>{params.contract}</p>
+						</div>
+						<div>
+							<h1 className="font-bold text-[28px]">{params.position}</h1>
+						</div>
+						<div>
+							<h1>{params.location}</h1>
+						</div>
 					</div>
 					<div>
-						<h1>{params.position}</h1>
+						<button className="bg-[#5964E0] text-white w-[141px] h-[48px] rounded-md">
+							Apply Now
+						</button>
 					</div>
-					<div>
-						<h1>{params.location}</h1>
-					</div>
+				</div>
+				<div className="pt-11">
 					<p>{params.description}</p>
 				</div>
-				<div>
+				<div className="pt-11">
 					<h1>Requirements</h1>
-					<p>{params.requirements.content}</p>
-					{params.requirements.items.map((item) => (
-						<p key={item}>{item}</p>
-					))}
+					<p className="pt-6 pb-11">{params.requirements.content}</p>
+					<ul className="list-disc">
+						{params.requirements.items.map((item) => (
+							<li key={item}>{item}</li>
+						))}
+					</ul>
 				</div>
 				<div>
-					<h1>What you will do</h1>
-					<p>{params.role.content}</p>
-					{params.role.items.map((item) => (
-						<p key={item}>{item}</p>
-					))}
+					<h1 className="pt-11">What you will do</h1>
+					<p className="pt-11">{params.role.content}</p>
+					<ul className="list-decimal pt-11">
+						{params.role.items.map((item) => (
+							<li className="" key={item}>
+								{item}
+							</li>
+						))}
+					</ul>
 				</div>
 			</div>
 			<div>Footer</div>
