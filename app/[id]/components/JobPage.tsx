60cb9e40bd01987416f809888f4b67cca666bdd6
Jared Brown <jaredbrown1@hotmail.com>
2024-01-11 20:10:11 -0800

    job page desktop styling completed

--- app/[id]/components/JobPage.tsx
+++ app/[id]/components/JobPage.tsx
@@ -33,15 +33,16 @@ const JobPage = ({ params }: JobPageProps) => {
 				<div className="flex justify-between">
 					<div>
 						{" "}
-						<div className="flex">
+						<div className="flex text-[#6e8098]">
 							<p>{params.postedAt}</p>
-							<p>{params.contract}</p>
+							<span className="pl-2 pr-2 text-[#5964e0]">•</span>
+							<p> {params.contract}</p>
 						</div>
 						<div>
 							<h1 className="font-bold text-[28px]">{params.position}</h1>
 						</div>
 						<div>
-							<h1>{params.location}</h1>
+							<h1 className="text-[#5964e0] font-bold">{params.location}</h1>
 						</div>
 					</div>
 					<div>
@@ -50,24 +51,31 @@ const JobPage = ({ params }: JobPageProps) => {
 						</button>
 					</div>
 				</div>
-				<div className="pt-11">
+				<div className="pt-11 text-[#6e8098]  text-[16px]">
 					<p>{params.description}</p>
 				</div>
 				<div className="pt-11">
 					<h1>Requirements</h1>
-					<p className="pt-6 pb-11">{params.requirements.content}</p>
-					<ul className="list-disc">
+					<p className="pt-6 pb-4 text-[#6e8098]">
+						{params.requirements.content}
+					</p>
+					<ul className="list-disc pl-4">
 						{params.requirements.items.map((item) => (
-							<li key={item}>{item}</li>
+							<li className="text-[#6e8098] marker:text-[#5964e0]" key={item}>
+								{item}
+							</li>
 						))}
 					</ul>
 				</div>
 				<div>
 					<h1 className="pt-11">What you will do</h1>
-					<p className="pt-11">{params.role.content}</p>
-					<ul className="list-decimal pt-11">
+					<p className="pt-11 text-[#6e8098]">{params.role.content}</p>
+					<ul className="list-decimal pt-11 pl-4">
 						{params.role.items.map((item) => (
-							<li className="" key={item}>
+							<li
+								className="text-[#6e8098] marker:text-[#5964e0] marker:font-bold pl-3"
+								key={item}
+							>
 								{item}
 							</li>
 						))}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Kumbh_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Kumbh_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
